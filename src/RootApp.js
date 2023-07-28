072f6f23c082747f19cd711a39b6c9676334cbe7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-28 13:50:26 +0900

    add splash view

--- src/RootApp.js
+++ src/RootApp.js
@@ -1,7 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootStackNavigation from "./navigations/RootStackNavigation";
+import { useState } from "react";
+import SplashView from "./SplashView";
 
 export default function RootApp() {
+  const [initialized, setInitialized] = useState(false);
+
+  if (!initialized) {
+    return <SplashView onFinishLoad={() => setInitialized(true)} />;
+  }
+
   return (
     <NavigationContainer>
       <RootStackNavigation />
