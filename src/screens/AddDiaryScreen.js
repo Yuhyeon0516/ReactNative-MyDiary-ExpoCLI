66c2b2e6e263b49ee232a15ccfe805eda994fcf6
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-31 17:13:06 +0900

    create image upload and date picker function in Add Diary screen

--- src/screens/AddDiaryScreen.js
+++ src/screens/AddDiaryScreen.js
@@ -1,12 +1,43 @@
-import { View } from "react-native";
-import { useCallback } from "react";
+import { ScrollView, View, useWindowDimensions } from "react-native";
+import { useCallback, useMemo, useState } from "react";
 import { Header } from "../components/Header/Header";
 import { useNavigation } from "@react-navigation/native";
+import { useImagePickAndUpload } from "../hooks/useImagePickAndUpload";
+import { CustomButton } from "../components/CustomButton";
+import { RemoteImage } from "../components/RemoteImage";
+import { Spacer } from "../components/Spacer";
+import { Typography } from "../components/Typography";
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 export default function AddDiaryScreen() {
+  const { width } = useWindowDimensions();
+  const photoSize = useMemo(() => {
+    return {
+      photoWidth: width,
+      photoHeight: width * 0.5,
+    };
+  }, [width]);
+
+  const [selectedPhotoUrl, setSelectedPhotoUrl] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const runImagePickAndUpload = useImagePickAndUpload();
+
+  const [visibleDatePicker, setVisibleDatePicker] = useState(false);
+
   const navigation = useNavigation();
   const onPressBack = useCallback(() => {
     navigation.goBack();
+  }, []);
+
+  const onPressPhotoItem = useCallback(async () => {
+    const result = await runImagePickAndUpload();
+    if (result.length >= 1) {
+      setSelectedPhotoUrl(result[0]);
+    }
+  }, []);
+
+  const onPressCalendar = useCallback(() => {
+    setVisibleDatePicker(true);
   }, []);
 
   return (
@@ -17,6 +48,35 @@ export default function AddDiaryScreen() {
         </Header.Group>
         <Header.Icon iconName={"close"} onPress={onPressBack} />
       </Header>
+      <ScrollView style={{ flex: 1 }}>
+        <CustomButton onPress={onPressPhotoItem}>
+          {selectedPhotoUrl ? (
+            <RemoteImage url={selectedPhotoUrl} width={photoSize.photoWidth} height={photoSize.photoHeight} />
+          ) : (
+            <View style={{ backgroundColor: "lightgray", width: photoSize.photoWidth, height: photoSize.photoHeight }} />
+          )}
+        </CustomButton>
+        <Spacer space={20} />
+        <CustomButton onPress={onPressCalendar}>
+          <View style={{ flexDirection: "row", paddingHorizontal: 24, paddingVertical: 12, alignItems: "center", justifyContent: "space-between" }}>
+            <Typography fontSize={20}>날짜</Typography>
+            <Typography fontSize={16}>
+              {selectedDate ? `${selectedDate.getFullYear()}. ${selectedDate.getMonth() + 1}. ${selectedDate.getDate()}` : "날짜를 선택해주세요."}
+            </Typography>
+          </View>
+        </CustomButton>
+      </ScrollView>
+      <DateTimePicker
+        isVisible={visibleDatePicker}
+        mode="date"
+        onConfirm={(date) => {
+          setSelectedDate(new Date(date));
+          setVisibleDatePicker(false);
+        }}
+        onCancel={() => {
+          setVisibleDatePicker(false);
+        }}
+      />
     </View>
   );
 }
