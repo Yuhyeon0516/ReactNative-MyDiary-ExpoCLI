59f1c77503c3c80432521a6c3cfa20f4286a3de6
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-31 16:19:18 +0900

    settings firebase

--- src/SplashView.js
+++ src/SplashView.js
@@ -2,29 +2,55 @@ import { Platform, View } from "react-native";
 import { GoogleSignin, GoogleSigninButton } from "@react-native-google-signin/google-signin";
 import { useCallback, useEffect, useState } from "react";
 import auth from "@react-native-firebase/auth";
+import database, { firebase } from "@react-native-firebase/database";
+import { useRecoilState } from "recoil";
+import { stateUserInfo } from "./states/stateUserInfo";
 
 export default function SplashView({ onFinishLoad }) {
   const [showLoginButton, setShowLoginButton] = useState(false);
+  const [_, setUserInfo] = useRecoilState(stateUserInfo);
   const signinUserIdentify = useCallback(async (idToken) => {
-    try {
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-      const result = await auth().signInWithCredential(googleCredential);
+    const result = await auth().signInWithCredential(googleCredential);
 
-      console.log(`${Platform.OS}`, result);
-    } catch (error) {
-      console.log(error);
+    const userDBRefKey = `/users/${result.user.uid}`;
+    const userResult = await database()
+      .ref(userDBRefKey)
+      .once("value")
+      .then((snapshot) => snapshot.val());
+
+    const now = new Date().toISOString();
+
+    if (!userResult) {
+      await database().ref(userDBRefKey).set({
+        name: result.additionalUserInfo.profile.name,
+        profileImage: result.additionalUserInfo.profile.picture,
+        uid: result.user.uid,
+        password: "",
+        createdAt: now,
+        lastLoginAt: now,
+      });
+    } else {
+      await database().ref(userDBRefKey).update({
+        lastLoginAt: now,
+      });
     }
+
+    const userInfo = await database()
+      .ref(userDBRefKey)
+      .once("value")
+      .then((snapshot) => snapshot.val());
+
+    setUserInfo(userInfo);
+
+    onFinishLoad();
   }, []);
 
   const onPressGoogleLogin = useCallback(async () => {
-    try {
-      await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-      const { idToken } = await GoogleSignin.signIn();
-      signinUserIdentify(idToken);
-    } catch (error) {
-      console.log(error);
-    }
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    const { idToken } = await GoogleSignin.signIn();
+    signinUserIdentify(idToken);
   }, []);
 
   const userSilentLogin = useCallback(async () => {
--- src/navigations/RootStackNavigation.js
+++ src/navigations/RootStackNavigation.js
@@ -8,7 +8,7 @@ export default function RootStackNavigation() {
   return (
     <Stack.Navigator initialRouteName="DiaryStack" screenOptions={{ headerShown: false, presentation: "containedModal" }}>
       <Stack.Screen name="DiaryStack" component={DiaryStackNavigation} />
-      <Stack.Screen name="AddDairy" component={AddDiaryScreen} />
+      <Stack.Screen name="AddDiary" component={AddDiaryScreen} />
     </Stack.Navigator>
   );
 }
