216779674b59b0dd3c5109ddfd235356b89e30f9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-31 14:26:44 +0900

    create google login function

--- src/SplashView.js
+++ src/SplashView.js
@@ -1,16 +1,46 @@
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { GoogleSignin, GoogleSigninButton } from "@react-native-google-signin/google-signin";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import auth from "@react-native-firebase/auth";
 
 export default function SplashView({ onFinishLoad }) {
+  const [showLoginButton, setShowLoginButton] = useState(false);
+  const signinUserIdentify = useCallback(async (idToken) => {
+    try {
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+      const result = await auth().signInWithCredential(googleCredential);
+
+      console.log(`${Platform.OS}`, result);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   const onPressGoogleLogin = useCallback(async () => {
-    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-    const { idToken } = await GoogleSignin.signIn();
+    try {
+      await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+      const { idToken } = await GoogleSignin.signIn();
+      signinUserIdentify(idToken);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const userSilentLogin = useCallback(async () => {
+    try {
+      const { idToken } = await GoogleSignin.signInSilently();
+      signinUserIdentify(idToken);
+    } catch (error) {
+      setShowLoginButton(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    userSilentLogin();
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <GoogleSigninButton onPress={onPressGoogleLogin} />
-    </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>{showLoginButton && <GoogleSigninButton onPress={onPressGoogleLogin} />}</View>
   );
 }
